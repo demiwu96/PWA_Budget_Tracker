7b17d11dabc91f64e8648d0fdd0a268254ced204
Demi Wu <demiwu96@gmail.com>
2020-06-03 11:52:15 -0400

    webpack and service worker config

---
+++ public/webpack.config.js
@@ -0,0 +1,58 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+    entry: {
+        index: "./index.js"
+    },
+    output: {
+        path: __dirname + "/dist",
+        filename: "[name].bundle.js"
+    },
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new WebpackPwaManifest({
+            name: "Budget Tracker App",
+            short_name: "Budget",
+            description: "An application that help you track your budget.",
+            background_color: "#09deed",
+            theme_color: "#ffffff",
+            start_url: "/",
+            icons: [{
+                src: path.resolve("icons/icon-192x192.png"),
+                sizes: [96, 128, 192, 256, 384, 512],
+                destination: path.join("assets", "icons")
+            }]
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            }
+        ]
+    }
+};
+
+module.exports = config;
